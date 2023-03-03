cdd18fafa901d2980fab360b935ada483a99712e
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-03 14:38:39 +0200

    Add files via upload

---
+++ 01. Count Real Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToList();
+
+Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+
+foreach (var number in numbers)
+{
+	if (!keyValuePairs.ContainsKey(number))
+	{
+		keyValuePairs.Add(number, 0);
+	}
+
+	keyValuePairs[number]++;
+}
+
+foreach (var item in keyValuePairs.OrderBy(x => x.Key))
+{
+	Console.WriteLine($"{item.Key} -> {item.Value}");
+}
