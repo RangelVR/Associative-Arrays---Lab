f878c148b3eb4d6ef38d85572d2d53d186f5002d
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-03 16:28:12 +0200

    Add files via upload

---
+++ 03. Word Synonyms/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> dictionatyOfSynonyms = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < n; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if (!dictionatyOfSynonyms.ContainsKey(word))
+    {
+        dictionatyOfSynonyms.Add(word, new List<string>());
+    }
+
+    dictionatyOfSynonyms[word].Add(synonym);
+}
+
+foreach (var word in dictionatyOfSynonyms)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
