3fe5d64bf44818955a34f1fb05c704c335fd3f3c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-03 15:54:36 +0200

    Add files via upload

---
+++ 02. Odd Occurrences/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string[] words = Console.ReadLine().ToLower().Split().ToArray();
+
+Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+foreach (var word in words)
+{
+	if (!wordsCount.ContainsKey(word))
+	{
+		wordsCount.Add(word, 0);
+	}
+	wordsCount[word]++;
+}
+
+wordsCount = wordsCount.Where(x => x.Value % 2 != 0).ToDictionary(x => x.Key,x => x.Value);
+
+foreach (var word in wordsCount)
+{
+	Console.Write("{0} ", word.Key);
+}
+
+//List<string> result = wordsCount.Where(x => x.Value % 2 != 0).Select(x => x.Key).ToList();
+//Console.WriteLine(string.Join(" ", result));
+
+//foreach (var word in wordsCount.Where(x => x.Value % 2 != 0))
+//{
+//	Console.Write("{0} ", word.Key);
+//}
