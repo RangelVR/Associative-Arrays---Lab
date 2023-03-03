de2fa374a0090610a251461237d9a51ca03450ad
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-03 16:43:07 +0200

    Add files via upload

---
+++ 04. Word Filter/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string[] words = Console.ReadLine()
+    .Split()
+    .Where(word => word.Length % 2 == 0)
+    .ToArray();
+
+Console.WriteLine(string.Join(Environment.NewLine, words));
+
+
+//Console.ReadLine()
+//   .Split()
+//   .Where(word => word.Length % 2 == 0)
+//   .ToList()
+//   .ForEach(Console.WriteLine);
+
